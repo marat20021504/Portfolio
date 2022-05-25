89ff1c410820bfcb0368b8bb3beb5cf7e7a1096d
marat20021504 <maratqochqorov@gmail.com>
2022-05-25 14:56:53 +0500

    the end

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(window).bind('scroll', function() {
+        let gap = 50;
+        if($(window).scrollTop > gap) {
+            $('header').addClass('active');
+        } else {
+            $('header').removeClass('active');   
+        }
+    })
+})
