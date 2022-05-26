24b6cfcf214a68222e056ffc4d67eadbd201fa79
marat20021504 <maratqochqorov@gmail.com>
2022-05-26 20:15:27 +0500

    add to language

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const menu = document.querySelector(".menu")
 const active = document.querySelector(".active")
 const ul = document.querySelector(".ul")
 
-menu.addEventListener("click", function() {
+function submit(){
     ul.classList.toggle("active")
-})
+}
